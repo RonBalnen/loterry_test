e366df8080043592f69d6c08cebedf111f085f30
Ron Balnen <ron.balnen@gmail.com>
2016-01-14 13:58:24 +0200

    Initial commit

---
+++ scripts/App.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import Slider from 'react-slick';
+import ReactPaginate from 'react-paginate';
+import data from "./data.json";
+import '../styles/style.css';
+
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    if (!localStorage.getItem("lotteryData")) {
+      localStorage.setItem("lotteryData", JSON.stringify(data));
+    }
+    this.data = JSON.parse(localStorage.getItem("lotteryData"));
+    console.log(this.data);
+    this.state = {
+      data: this.data.slice(10,49),
+      offset: 10,
+      limit: 40
+    };
+
+  }
+
+  loadsLottery() {
+    const { offset, limit } = this.state;
+    this.setState({data: this.data.slice(offset, offset+limit), pageNum: Math.ceil(this.data.length / limit)});
+  }
+
+  handlePageClick (data)  {
+    console.log("handle", data);
+    let selected = data.selected;
+    let offset = Math.ceil(selected * this.state.limit);
+
+    this.setState({offset: offset}, () => {
+      this.loadsLottery();
+    });
+  }
+
+  render() {
+    const {data} = this.state;
+    console.log("state", this.state);
+    const sliderData = this.data.slice(0, 9).map(item => (<div><h3>Winning number: {item.winning_numbers}</h3></div>));
+    console.log(sliderData);
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div className="container">
+        <div className="slider-container">
+          <Slider {...settings}>
+            {sliderData}
+          </Slider>
+        </div>
+        {data.map(i => (
+          <div className="list">
+            <p>Draw date: {i.draw_date}</p><p>Winning number: {i.winning_numbers}</p>
+            <hr />
+          </div>
+        ))}
+        <div className="react-paginate">
+          <ReactPaginate  previousLabel={"previous"}
+                       nextLabel={"next"}
+                       breakLabel={<li className="break"><a href="">...</a></li>}
+                       pageNum={this.state.pageNum}
+                       marginPagesDisplayed={2}
+                       pageRangeDisplayed={5}
+                       clickCallback={this.handlePageClick.bind(this)}
+                       containerClassName={"pagination"}
+                       subContainerClassName={"pages pagination"}
+                       activeClassName={"active"} />
+        </div>
+      </div>
+
+    );
+  }
+}
+
